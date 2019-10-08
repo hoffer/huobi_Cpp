c08f9f7156b0f14278bab531bc6e0755934db51f
Hoffer <hoffer1127@gmail.com>
2019-10-08 00:32:29 -0700

    add local timestamp
    Signed-off-by: Hoffer <hoffer1127@gmail.com>

--- examples/SubscribePriceDepth/main.cpp
+++ examples/SubscribePriceDepth/main.cpp
@@ -3,18 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+#include <time.h>
 #include <iostream>
 #include "Huobi/HuobiClient.h"
 
 using namespace Huobi;
 using namespace std;
+time_t timer;
 
 int main(int argc, char** argv) {
     SubscriptionOptions options;
     options.url = "wss://api.huobi.pro";
     SubscriptionClient* client = createSubscriptionClient(options); 
-    client->subscribePriceDepthEvent("btcusdt", [](PriceDepthEvent priceDepthEvent) {      
+    client->subscribePriceDepthEvent("btcusdt", [](PriceDepthEvent priceDepthEvent) {
+        time(&timer);
+        cout << "Local: " << timer << "  Server: " << priceDepthEvent.timestamp << endl;
         cout << "Symbol: " << priceDepthEvent.symbol << endl;
         cout << "Bid price: " << priceDepthEvent.data.bids[0].price<< endl;
         cout << "Ask price: " << priceDepthEvent.data.asks[0].price << endl;
